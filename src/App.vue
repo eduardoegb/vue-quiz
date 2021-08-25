8cb524e6efbc18adbd928417451833d38f0513b4
Eduardo Gonzalez <eduardoegb@gmail.com>
2021-08-24 20:36:25 -0400

    Creating Questions and Result components, moving between questions

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,123 @@
 <template>
   <div class="ctr">
-    <div class="questions-ctr">
-      <div class="progress">
-        <div class="bar"></div>
-        <div class="status">1 out of 3 questions answered</div>
-      </div>
-      <div class="single-question">
-        <div class="question">Sample Question 1</div>
-        <div class="answers">
-          <div class="answer">Sample Answer 1</div>
-          <div class="answer">Sample Answer 2</div>
-          <div class="answer">Sample Answer 3</div>
-          <div class="answer">Sample Answer 4</div>
-        </div>
-      </div>
-    </div>
-    <div class="result">
-      <div class="title">You got sample result 1!</div>
-      <div class="desc">Enter a short description here about the result.</div>
-    </div>
+    <questions
+      v-if="questionsAnswered < questions.length"
+      :questions="questions"
+      :questionsAnswered="questionsAnswered"
+      @question-answered="onQuestionAnswered"
+    />
+    <result v-else />
     <button type="button" class="reset-btn">Reset</button>
   </div>
 </template>
 
 <script>
+  import Questions from "./components/Questions.vue";
+  import Result from "./components/Result.vue";
+
   export default {
     name: "App",
+    components: { Questions, Result },
+    data() {
+      return {
+        questionsAnswered: 0,
+        correctAnswers: 0,
+        questions: [
+          {
+            q: "What is 2 + 2?",
+            id: "1",
+            answers: [
+              {
+                text: "4",
+                id: "1a",
+                is_correct: true,
+              },
+              {
+                text: "3",
+                id: "1b",
+                is_correct: false,
+              },
+              {
+                text: "Fish",
+                id: "1c",
+                is_correct: false,
+              },
+              {
+                text: "5",
+                id: "1d",
+                is_correct: false,
+              },
+            ],
+          },
+          {
+            q: 'How many letters are in the word "Banana"?',
+            id: "2",
+            answers: [
+              {
+                text: "5",
+                id: "2a",
+                is_correct: false,
+              },
+              {
+                text: "7",
+                id: "2b",
+                is_correct: false,
+              },
+              {
+                text: "6",
+                id: "2c",
+                is_correct: true,
+              },
+              {
+                text: "12",
+                id: "2d",
+                is_correct: false,
+              },
+            ],
+          },
+          {
+            q: "Find the missing letter: C_ke",
+            id: "3",
+            answers: [
+              {
+                text: "e",
+                id: "3a",
+                is_correct: false,
+              },
+              {
+                text: "a",
+                id: "3b",
+                is_correct: true,
+              },
+              {
+                text: "i",
+                id: "3c",
+                is_correct: false,
+              },
+            ],
+          },
+        ],
+        results: [
+          {
+            min: 0,
+            max: 2,
+            title: "Try again!",
+            desc: "Do a little more studying and you may succeed!",
+          },
+          {
+            min: 3,
+            max: 3,
+            title: "Wow, you're a genius!",
+            desc: "Studying has definitely paid off for you!",
+          },
+        ],
+      };
+    },
+    methods: {
+      onQuestionAnswered(is_correct) {
+        if (is_correct) this.correctAnswers++;
+        this.questionsAnswered++;
+      },
+    },
   };
 </script>
-
